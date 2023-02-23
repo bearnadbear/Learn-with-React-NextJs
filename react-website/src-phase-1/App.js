e85bc00100ad8cc42acf6ed33cc9865864d546cf
farisfirdausapr <punyanyaarii@gmail.com>
2023-02-24 02:46:53 +0700

    Adding Routing

---
+++ react-website/src-phase-1/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+    return (
+        <div>
+            <h1>My Todos</h1>
+
+            <Todo text='Learn with React 1'/>
+            <Todo text='Learn with React 2'/>
+        </div>
+    );
+}
+
+export default App;
--- react-website/src/App.js
+++ react-website/src/App.js
@@ -1,14 +1,29 @@
-import Todo from "./components/Todo";
+import {Route, Switch} from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupsPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
 
 function App() {
     return (
         <div>
-            <h1>My Todos</h1>
+            <h1>Routing Course</h1>
 
-            <Todo text='Learn with React 1'/>
-            <Todo text='Learn with React 2'/>
+            <Switch>
+                <Route path='/' exact>
+                    <AllMeetupsPage/>
+                </Route>
+
+                <Route path='/new-meetups'>
+                    <NewMeetupsPage/>
+                </Route>
+
+                <Route path='/favorites'>
+                    <FavoritesPage/>
+                </Route>
+            </Switch>
         </div>
     );
-}
+};
 
 export default App;
