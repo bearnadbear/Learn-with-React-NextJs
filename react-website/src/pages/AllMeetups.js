3c5c3ec79434a820a53f6fad66a1d158728ff40f
farisfirdausapr <punyanyaarii@gmail.com>
2023-02-24 11:05:50 +0700

    Adding Even More Components

--- react-website/src/pages/AllMeetups.js
+++ react-website/src/pages/AllMeetups.js
@@ -1,5 +1,37 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DummyData = [
+    {
+        id: 'm1',
+        title: 'This is a first meetup',
+        image: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/enhancer/2.jpg',
+        address: 'Meetupsteet 5, 12345 Meetup City',
+        description: 'Good job'
+    }, {
+        id: 'm2',
+        title: 'This is a next meetup',
+        image: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/enhancer/2.jpg',
+        address: 'Meetupsteet 5, 12345 Meetup City',
+        description: 'Good job ....'
+    }
+]
+
 function AllMeetupsPage() {
-    return <div>All Meetups Page</div>;
+    return (
+        <section>
+            <h1>All Meetups</h1>
+
+            <MeetupList meetups={DummyData}/>
+            {/* <ul>
+                {
+                    DummyData.map((meetup) => {
+                        return <li key={meetup.id}>{meetup.title}
+                        </li>
+                    })
+                }
+            </ul> */}
+        </section>
+    );
 };
 
 export default AllMeetupsPage;
