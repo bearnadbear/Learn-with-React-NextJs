d367dd30205990cf492007fd7b41d1f1cf38fe61
farisfirdausapr <punyanyaarii@gmail.com>
2023-02-24 11:13:30 +0700

    Adding Even More Components

--- react-website/src/components/layout/MainNavigation.js
+++ react-website/src/components/layout/MainNavigation.js
@@ -1,6 +1,6 @@
 import {Link} from "react-router-dom";
 
-import classes from './MainNavigation.module.css'
+import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
     return (
