aafcaa44e62099b2eeda977734feea597bdb256b
farisfirdausapr <punyanyaarii@gmail.com>
2023-02-27 10:36:19 +0700

    Navigating Programmatically

--- nextjs-website/pages/clients/index.js
+++ nextjs-website/pages/clients/index.js
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 export default function ClientsPage(){
+    const clients = [
+        {id: "max", name: "Maximilian"},
+        {id: "min", name: "Minimalian"}
+    ];
+
     return (
         <div>
             <h1>The Clients Page</h1>
+        
+            <ul>
+                {clients.map(client => <li key={client.id}>
+                    <Link href={`/clients/${client.id}`}>
+                        {client.name}
+                    </Link>
+                </li>)}
+            </ul>
         </div>
     );
 };
--- nextjs-website/pages/index.js
+++ nextjs-website/pages/index.js
@@ -1,7 +1,19 @@
-export default function HomePage (){
-  return (
-    <div>
-    <h1>The Home Page</h1>
-    </div>
-  );
+import Link from 'next/link';
+
+export default function HomePage() {
+    return (
+        <div>
+            <h1>The Home Page</h1>
+
+            <ul>
+                <li>
+                    <Link href='/portfolio'>Portfolio</Link>
+                </li>
+
+                <li>
+                    <Link href='/clients'>Clients</Link>
+                </li>
+            </ul>
+        </div>
+    );
 };
