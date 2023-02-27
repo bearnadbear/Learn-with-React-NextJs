29458a9f50984d379dfd676f7eff47b4616b11fc
farisfirdausapr <punyanyaarii@gmail.com>
2023-02-27 10:41:07 +0700

    A Diffrent Way of Setting Link Hrefs

--- nextjs-website/pages/clients/index.js
+++ nextjs-website/pages/clients/index.js
@@ -12,7 +12,10 @@ export default function ClientsPage(){
         
             <ul>
                 {clients.map(client => <li key={client.id}>
-                    <Link href={`/clients/${client.id}`}>
+                    <Link href={{
+                        pathname: '/clients/[id]',
+                        query: {id: client.id}
+                    }}>
                         {client.name}
                     </Link>
                 </li>)}
