92a18bc1f2ccb684aa2b8ea4cefcdfb0e03b54a0
farisfirdausapr <punyanyaarii@gmail.com>
2023-02-27 16:09:05 +0700

    Styling Components In Next.js Projects

--- nextjs-website/components/events/event-list.js
+++ nextjs-website/components/events/event-list.js
@@ -1,10 +1,12 @@
 import EventItem from './event-item';
 
+import classes from './event-list.module.css';
+
 export default function EventList(props) {
     const {items} = props;
 
     return (
-        <ul>
+        <ul className={classes.list}>
             {
                 items.map(event => (
                     <EventItem
@@ -15,7 +17,7 @@ export default function EventList(props) {
                         date={event.date}
                         image={event.image}/>
                 ))
-            };
+            }
         </ul>
     );
 };
