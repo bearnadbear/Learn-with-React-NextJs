f31a815bc9896920372bded3f04d7285fbde7740
farisfirdausapr <punyanyaarii@gmail.com>
2023-03-10 17:18:01 +0700

    Using API Routes to Get Data

--- nextjs-website-2/pages/index.js
+++ nextjs-website-2/pages/index.js
@@ -1,6 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function HomePage() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+
   const emailInputRef = useRef();
   const feedbackInputRef = useRef();
 
@@ -23,23 +25,35 @@ export default function HomePage() {
       .then((data) => console.log(data));
   }
 
+  function loadFeedbackHandler() {
+    fetch("/api/feedback")
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackItems(data.feedback);
+      });
+  }
+
   return (
     <div>
-      <h1>Home Page!</h1>
-
-      <form onClick={submitFormHandler}>
+      <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
         <div>
           <label htmlFor="email">Your Email Address</label>
-          <input id="email" type="email" ref={emailInputRef} />
+          <input type="email" id="email" ref={emailInputRef} />
         </div>
-
         <div>
           <label htmlFor="feedback">Your Feedback</label>
-          <textarea type="text" rows="4" ref={feedbackInputRef}></textarea>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
         </div>
-
         <button>Send Feedback</button>
       </form>
+      <hr />
+      <button onClick={loadFeedbackHandler}>Load Feedback</button>
+      <ul>
+        {feedbackItems.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
