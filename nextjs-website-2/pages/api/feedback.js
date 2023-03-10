2dccb3157c8e92e53c058264a7cd51700a2f6648
farisfirdausapr <punyanyaarii@gmail.com>
2023-03-10 16:51:31 +0700

    Parsing The Incoming Request & Executing Server-side Code . Sending Requests To API Routes

--- nextjs-website-2/pages/api/feedback.js
+++ nextjs-website-2/pages/api/feedback.js
@@ -1,3 +1,26 @@
+import fs from "fs";
+import path from "path";
+
 export default function handler(req, res) {
-  res.status(200).json("Hallo semua");
+  if (req.method === "POST") {
+    const email = req.body.email;
+    const feedbackText = req.body.text;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email: email,
+      text: feedbackText,
+    };
+
+    const filePath = path.join(process.cwd(), "data", "feedback.json");
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData);
+
+    data.push(newFeedback);
+
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({ message: "Created!", feedback: newFeedback });
+  }
+
+  res.status(200).json({ message: "Success!" });
 }
--- nextjs-website-2/pages/index.js
+++ nextjs-website-2/pages/index.js
@@ -1,29 +1,41 @@
 import { useRef } from "react";
 
 export default function HomePage() {
-  const emailRef = useRef();
-  const feedbackRef = useRef();
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
 
-  function confirmHancdler(e) {
-    e.preventDefault();
+  function submitFormHandler(event) {
+    event.preventDefault();
 
-    const enteredEmail = emailRef.current.value;
-    const enteredFeedback = feedbackRef.current.value;
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    const reqBody = { email: enteredEmail, text: enteredFeedback };
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   }
 
   return (
     <div>
       <h1>Home Page!</h1>
 
-      <form onClick={confirmHancdler}>
+      <form onClick={submitFormHandler}>
         <div>
           <label htmlFor="email">Your Email Address</label>
-          <input id="email" type="email" ref={emailRef} />
+          <input id="email" type="email" ref={emailInputRef} />
         </div>
 
         <div>
           <label htmlFor="feedback">Your Feedback</label>
-          <textarea type="text" rows="4" ref={feedbackRef}></textarea>
+          <textarea type="text" rows="4" ref={feedbackInputRef}></textarea>
         </div>
 
         <button>Send Feedback</button>
